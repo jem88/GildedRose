d9ac8368e824dc64302e552f884447da2750db77
jem88 <marino.meneghel@gmail.com>
2015-04-08 14:20:26 +0200

    Added an update method for every product and some helper methods

--- src/main/java/com/gildedrose/GildedRose.java
+++ src/main/java/com/gildedrose/GildedRose.java
@@ -15,6 +15,75 @@ class GildedRose {
         OTHERS
     }
 
+
+    public void updateQuality() {
+        for (Item item : items) {
+            switch( getItemType(item.name) ) {
+                case AGED_BRIE:
+                    updateAgedBrie(item);
+                    break;
+                case BACKSTAGE_PASSES:
+                    updateBackstagePasses(item);
+                    break;
+                case SULFURAS:
+                    // Nothing to do here.
+                    break;
+                case CONJURED:
+                    updateConjured(item);
+                    break;
+                case OTHERS:
+                    updateOthersProducts(item);
+                    break;
+            }
+        }
+    }
+
+    private void updateOthersProducts(Item item) {
+        decrementQuality(item, 1);
+        decrementSellIn(item);
+    }
+
+    private void updateConjured(Item item) {
+        decrementQuality(item, 2);
+        decrementSellIn(item);
+    }
+
+    private void updateBackstagePasses(Item item) {
+        decrementSellIn(item);
+        incrementQuality(item, 1);
+        if (item.sellIn < 6) {
+            incrementQuality(item, 2);
+        } else if (item.sellIn < 11) {
+            incrementQuality(item, 1);
+        }
+        if (item.sellIn < 0) {
+            item.quality = 0;
+        }
+    }
+
+    private void updateAgedBrie(Item item) {
+        incrementQuality(item, 1);
+        decrementSellIn(item);
+    }
+
+    private void incrementQuality(Item item, int incrementOf) {
+        if (item.quality < 50)
+            item.quality = item.quality + incrementOf;
+    }
+
+
+    private void decrementQuality(Item item, int decrementOf) {
+        if (item.quality > 0)
+            item.quality = item.quality - decrementOf;
+        if (item.sellIn < 0 && item.quality > 0)
+            item.quality = item.quality - 1;
+    }
+
+    private void decrementSellIn(Item item) {
+        item.sellIn = item.sellIn - 1;
+    }
+
+
     private itemType getItemType(String itemName) {
         if (itemName.equals("Aged Brie")) {
             return itemType.AGED_BRIE;
@@ -27,54 +96,5 @@ class GildedRose {
         } else {
             return itemType.OTHERS;
         }
-    }
-
-
-    public void updateQuality() {
-        for (Item item : items) {
-
-            switch( getItemType(item.name) ) {
-                case AGED_BRIE:
-                    incrementQuality(item, 1);
-                    item.sellIn = item.sellIn - 1;
-                    break;
-                case BACKSTAGE_PASSES:
-                    item.sellIn = item.sellIn - 1;
-                    incrementQuality(item, 1);
-                    if (item.sellIn < 11) {
-                        incrementQuality(item, 1);
-                    }
-                    if (item.sellIn < 6) {
-                        incrementQuality(item, 1);
-                    }
-                    if (item.sellIn < 0) {
-                        item.quality = 0;
-                    }
-                    break;
-                case SULFURAS:
-
-                    break;
-                case CONJURED:
-                    decrementQuality(item, 2);
-                    item.sellIn = item.sellIn - 1;
-                    break;
-                case OTHERS:
-                    decrementQuality(item, 1);
-                    item.sellIn = item.sellIn - 1;
-                    break;
-            }
-        }
-    }
-
-    private void decrementQuality(Item item, int decrementOf) {
-        if (item.quality > 0)
-            item.quality = item.quality - decrementOf;
-        if (item.sellIn < 0 && item.quality > 0)
-            item.quality = item.quality - 1;
-    }
-
-    private void incrementQuality(Item item, int incrementOf) {
-        if (item.quality < 50)
-            item.quality = item.quality + incrementOf;
     }
 }
