da883cdaada8089bcf182d72e527e8784662afd3
jem88 <marino.meneghel@gmail.com>
2015-04-08 14:08:41 +0200

    Started GildedRose class refactoring

--- src/main/java/com/gildedrose/GildedRose.java
+++ src/main/java/com/gildedrose/GildedRose.java
@@ -7,61 +7,74 @@ class GildedRose {
         this.items = items;
     }
 
+    private enum itemType {
+        AGED_BRIE,
+        SULFURAS,
+        CONJURED,
+        BACKSTAGE_PASSES,
+        OTHERS
+    }
+
+    private itemType getItemType(String itemName) {
+        if (itemName.equals("Aged Brie")) {
+            return itemType.AGED_BRIE;
+        } else if (itemName.equals("Backstage passes to a TAFKAL80ETC concert")) {
+            return itemType.BACKSTAGE_PASSES;
+        } else if (itemName.equals("Sulfuras, Hand of Ragnaros")) {
+            return itemType.SULFURAS;
+        } else if (itemName.equals("Conjured")) {
+            return itemType.CONJURED;
+        } else {
+            return itemType.OTHERS;
+        }
+    }
+
+
     public void updateQuality() {
-        for (int i = 0; i < items.length; i++) {
-            if (!items[i].name.equals("Aged Brie")
-                    && !items[i].name.equals("Backstage passes to a TAFKAL80ETC concert")) {
+        for (Item item : items) {
 
-                if (items[i].quality > 0) {
-                    if (!items[i].name.equals("Sulfuras, Hand of Ragnaros")) {
-                        items[i].quality = items[i].quality - 1;
+            switch( getItemType(item.name) ) {
+                case AGED_BRIE:
+                    incrementQuality(item, 1);
+                    item.sellIn = item.sellIn - 1;
+                    break;
+                case BACKSTAGE_PASSES:
+                    item.sellIn = item.sellIn - 1;
+                    incrementQuality(item, 1);
+                    if (item.sellIn < 11) {
+                        incrementQuality(item, 1);
                     }
-                    if (items[i].name.equals("Conjured")) {
-                        items[i].quality = items[i].quality - 1;
+                    if (item.sellIn < 6) {
+                        incrementQuality(item, 1);
                     }
-                 }
-
-            } else {
-                if (items[i].quality < 50) {
-                    items[i].quality = items[i].quality + 1;
-
-                    if (items[i].name.equals("Backstage passes to a TAFKAL80ETC concert")) {
-                        if (items[i].sellIn < 11) {
-                            if (items[i].quality < 50) {
-                                items[i].quality = items[i].quality + 1;
-                            }
-                        }
-
-                        if (items[i].sellIn < 6) {
-                            if (items[i].quality < 50) {
-                                items[i].quality = items[i].quality + 1;
-                            }
-                        }
+                    if (item.sellIn < 0) {
+                        item.quality = 0;
                     }
-                }
-            }
+                    break;
+                case SULFURAS:
 
-            if (!items[i].name.equals("Sulfuras, Hand of Ragnaros")) {
-                items[i].sellIn = items[i].sellIn - 1;
-            }
-
-            if (items[i].sellIn < 0) {
-                if (!items[i].name.equals("Aged Brie")) {
-                    if (!items[i].name.equals("Backstage passes to a TAFKAL80ETC concert")) {
-                        if (items[i].quality > 0) {
-                            if (!items[i].name.equals("Sulfuras, Hand of Ragnaros")) {
-                                items[i].quality = items[i].quality - 1;
-                            }
-                        }
-                    } else {
-                        items[i].quality = items[i].quality - items[i].quality;
-                    }
-                } else {
-                    if (items[i].quality < 50) {
-                        items[i].quality = items[i].quality + 1;
-                    }
-                }
+                    break;
+                case CONJURED:
+                    decrementQuality(item, 2);
+                    item.sellIn = item.sellIn - 1;
+                    break;
+                case OTHERS:
+                    decrementQuality(item, 1);
+                    item.sellIn = item.sellIn - 1;
+                    break;
             }
         }
+    }
+
+    private void decrementQuality(Item item, int decrementOf) {
+        if (item.quality > 0)
+            item.quality = item.quality - decrementOf;
+        if (item.sellIn < 0 && item.quality > 0)
+            item.quality = item.quality - 1;
+    }
+
+    private void incrementQuality(Item item, int incrementOf) {
+        if (item.quality < 50)
+            item.quality = item.quality + incrementOf;
     }
 }
